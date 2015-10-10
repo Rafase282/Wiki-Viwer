d69ca7fc6b5eff66e6a7eb63e9bfd7b2de70ce79
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-10 01:06:28 -0400

    Done

--- js/index.js
+++ js/index.js
@@ -1,43 +1,40 @@
-var url = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=rafael';
-var query = $('#search').val();
+var arrResults = [];
+var html = '';
+
+function Result(title, snippet) {
+  this.title = title;
+  this.snippet = snippet;
+}
 
 function search() {
-
-  // Option 1
-  $.getJSON(url + query, function(response) {
-    console.log(data);
-    var title = data.query.pages[0].title;
-    console.log(title);
-  });
-
-  // Option 2
   $.ajax({
-    url: url,
-    action: 'query',
-    list: 'search',
-    format: 'json',
-    srsearch: query,
+    url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#search').val(),
     dataType: 'jsonp',
     type: 'POST',
     headers: {
       'Api-User-Agent': 'Example/1.0'
     },
     success: function(data) {
+      $('.results').empty();
       // do something with data
-      console.log(data);
-      var title = data.query.pages[0].title;
-      console.log(title);
+      arrResults.length = 0;
+      var resArr = data.query.search;
+      for (var result in resArr) {
+        arrResults.push(new Result(resArr[result].title, resArr[result].snippet));
+        html = '<div id="articles" class="well"><a href="https://en.wikipedia.org/wiki/' + resArr[result].title + '"target="_blank"><h3>' + resArr[result].title + '</h3><p>' + resArr[result].snippet + '</p></a></div>';
+
+        // Displays the elements to the page
+        $('.results').append(html);
+      }
     }
   });
 
   if ($('#search').val().length > 0) {
-    // Display matching names by hiding anything that is not what we want from the  class= "user"
-    var reg = new RegExp($('#search').val(), 'ig');
-    $('.user').css('display', 'none');
+    $('.articles').css('display', 'none');
 
   } else if ($('#search').val().length < 1) {
     // display everything again
-    $('.user').css('display', 'block');
+    $('.articles').css('display', 'block');
   }
 
   $('#search').unbind('keyup');
