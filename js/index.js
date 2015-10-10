318686f3f512c10464296e8b34c00f69a16b4284
Rafael J. Rodriguez <rafase282@gmail.com>
2015-10-10 01:07:10 -0400

    Done

---
+++ js/index.js
@@ -0,0 +1,48 @@
+var arrResults = [];
+var html = '';
+
+function Result(title, snippet) {
+  this.title = title;
+  this.snippet = snippet;
+}
+
+function search() {
+  $.ajax({
+    url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#search').val(),
+    dataType: 'jsonp',
+    type: 'POST',
+    headers: {
+      'Api-User-Agent': 'Example/1.0'
+    },
+    success: function(data) {
+      $('.results').empty();
+      // do something with data
+      arrResults.length = 0;
+      var resArr = data.query.search;
+      for (var result in resArr) {
+        arrResults.push(new Result(resArr[result].title, resArr[result].snippet));
+        html = '<div id="articles" class="well"><a href="https://en.wikipedia.org/wiki/' + resArr[result].title + '"target="_blank"><h3>' + resArr[result].title + '</h3><p>' + resArr[result].snippet + '</p></a></div>';
+
+        // Displays the elements to the page
+        $('.results').append(html);
+      }
+    }
+  });
+
+  if ($('#search').val().length > 0) {
+    $('.articles').css('display', 'none');
+
+  } else if ($('#search').val().length < 1) {
+    // display everything again
+    $('.articles').css('display', 'block');
+  }
+
+  $('#search').unbind('keyup');
+  $('#search').keyup(function() {
+    search();
+  });
+}
+
+$('#search').keyup(function() {
+  search();
+});
